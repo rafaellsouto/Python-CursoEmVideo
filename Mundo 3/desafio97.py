b0fd5fa6820b254ffb87fdeca202556f6d7e8156
Rafael <m4718@mersan.local>
2025-01-21 09:57:11 -0300

    97 melhorado

--- Mundo 3/desafio97.py
+++ Mundo 3/desafio97.py
@@ -1,7 +1,7 @@
 def escreva(msg):
-    print('~' * len(msg))
-    print(msg)
-    print('~' * len(msg))
+    print('~' * (len(msg) + 4))
+    print(f'  {msg}')
+    print('~' * (len(msg) + 4))
 
 
 escreva('THEOTOKOS')
