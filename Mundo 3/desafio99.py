41d44ff5c9a12b4cb2061c54c789bb93816378cf
Rafael <m4718@mersan.local>
2025-01-21 11:21:10 -0300

    99 completo

--- Mundo 3/desafio99.py
+++ Mundo 3/desafio99.py
@@ -1,15 +1,19 @@
 from time import sleep
 
 def analise(*num):
+    if num == None:
+        maior = 0
+        tam = 0
     print('-' * 30)
+    tam = len(num)
     print('Valores passados: ')
     for v in num:
         print(f'{v} ', end='')
         sleep(0.5)
     print()
 
-    print(f'Foram informados {len(num)} valores.')
-    maior = num[0]
+    print(f'Foram informados {tam} valores.')
+    maior = 0 # num[0]
     for valor in num:
         if valor > maior:
             maior = valor
@@ -21,3 +25,4 @@ analise(4, 5, 6)
 analise(4, 12, 5, 35, 6)
 analise(14, 5, 60, 27)
 analise(1, 7)
+analise()
